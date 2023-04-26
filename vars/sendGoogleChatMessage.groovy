be7d9803bd2efac50130ed490f628dd740b161d2
Simon Kepplinger <simon.kepplinger@valcome.at>
2023-04-26 13:20:53 +0200

    Hack 🌚?

--- vars/sendGoogleChatMessage.groovy
+++ vars/sendGoogleChatMessage.groovy
@@ -1,11 +1,9 @@
-import groovy.json.StringEscapeUtils
-
 def call(message) {
     withCredentials([string(credentialsId: 'GOOGLE_CHAT_JENKINS_KEY', variable: 'SECRET_VALUE')]) {
         sh '''
         curl \
         -H "Content-Type: application/json; charset=UTF-8" \
-        --data "{'text': \'''' + StringEscapeUtils.escapeJava(message) + '''\'}" \
+        --data "{'text': \'''' + "${message}" + '''\'}" \
         -X POST "https://chat.googleapis.com/v1/spaces/AAAA5SJWKzg/messages?key=$SECRET_VALUE"
         '''
     }
