6041ce68ebebfeea121c594883f1ef63070a9b5d
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2023-05-22 15:53:41 +0200

    Update publishChecks function

--- vars/publishChecks.groovy
+++ vars/publishChecks.groovy
@@ -1,8 +1,16 @@
-def call(name, title, summary, text, status, detailsURL) {
+
+def call(name,
+         title,
+         status, // NONE, QUEUED, IN_PROGRESS, COMPLETED
+         conclusion, // NONE, ACTION_REQUIRED, SKIPPED, CANCELED, TIME_OUT, FAILURE, NEUTRAL, SUCCESS
+         summary,
+         text,
+         detailsURL) {
     publishChecks name      : name,
                   text      : text,
                   title     : title,
                   status    : status,
                   summary   : summary,
+                  conclusion: conclusion,
                   detailsURL: detailsURL
 }
