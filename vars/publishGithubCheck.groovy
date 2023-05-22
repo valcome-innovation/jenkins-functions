867f349b6e3e650d044e1ce6b9b48879322a99d5
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2023-05-22 16:01:28 +0200

    Add default parameters

--- vars/publishGithubCheck.groovy
+++ vars/publishGithubCheck.groovy
@@ -1,11 +1,10 @@
-
 def call(name,
          title,
-         status, // NONE, QUEUED, IN_PROGRESS, COMPLETED
-         conclusion, // NONE, ACTION_REQUIRED, SKIPPED, CANCELED, TIME_OUT, FAILURE, NEUTRAL, SUCCESS
-         summary,
-         text,
-         detailsURL) {
+         status = 'COMPLETED', // NONE, QUEUED, IN_PROGRESS, COMPLETED
+         conclusion = 'SUCCESS', // NONE, ACTION_REQUIRED, SKIPPED, CANCELED, TIME_OUT, FAILURE, NEUTRAL, SUCCESS
+         summary = '',
+         text = '',
+         detailsURL = '') {
     publishChecks name      : name,
                   text      : text,
                   title     : title,
