c81e90ce281021cb7ac9b32ae864901e7a5b5482
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2023-06-12 11:35:41 +0200

    Fix google chat message syntax error in curl

--- vars/sendGoogleChatMessage.groovy
+++ vars/sendGoogleChatMessage.groovy
@@ -4,7 +4,7 @@ def call(message) {
         curl \
         --silent \
         --show-error \
-        -o /dev/null
+        -o /dev/null \
         -H "Content-Type: application/json; charset=UTF-8" \
         --data '{"text": \"''' + message + '''\"'} \
         -X POST "https://chat.googleapis.com/v1/spaces/AAAA5SJWKzg/messages?key=$SECRET_VALUE" \
