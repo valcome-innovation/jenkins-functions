bbefdaa225c0e2bf89a7940218f32ae65fdd4f6a
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2023-06-12 09:02:25 +0200

    add AWSCodebuild utility class

---
+++ src/org/valcome/AWSCodebuild.groovy
@@ -0,0 +1,67 @@
+package org.valcome
+
+buildParams
+
+def construct(project = null, branch = 'main', version = null, app = null, environment = null) {
+    buildParams = [
+        project: project,
+        branch: branch,
+        version: version,
+        app: app,
+        environment: environment
+    ]
+}
+
+
+def triggerBuild(remote) {
+    def build = startBuild(remote)
+    def runningBuild = getBuildStatus(remote, build.id)
+
+    while (runningBuild.buildBatchStatus == "IN_PROGRESS") {
+        sleep 15
+        runningBuild = getBuildStatus(remote, build.id)
+        echo "Phase: " + runningBuild.currentPhase
+        echo "Status: " + runningBuild.buildBatchStatus
+        echo "wait 15 seconds for next poll..."
+    }
+
+    if (runningBuild.buildBatchStatus == "STOPPED") {
+        currentBuild.result = 'ABORTED'
+    } else if (runningBuild.buildBatchStatus != "SUCCEEDED") {
+        throw new Exception('AWS Code Build failed (https://eu-central-1.console.aws.amazon.com/codesuite/codebuild/projects?region=eu-central-1)')
+    }
+}
+
+def startBuild(remote) {
+    def customCommand = """
+        aws codebuild start-build-batch \
+            --project-name ${buildParams.project} \
+            --source-version refs/heads/${buildParams.branch} \
+            --environment-variables-override
+    """
+
+    if (buildParams.app != null) {
+        customCommand.concat(" name=APP,value=${buildParams.app} ")
+    }
+
+    if (buildParams.version != null) {
+        customCommand.concat(" name=TAG,value=${buildParams.version} ")
+    }
+
+    if (buildParams.environment != null) {
+        customCommand.concat(" name=ENVIRONMENT,value=${buildParams.app} ")
+    }
+
+
+    def result = sshCommand remote: remote, command: customCommand
+    def json = readJSON text: "" + result
+    return json.buildBatch
+}
+
+def getBuildStatus(remote, build_id) {
+    def result = sshCommand remote: remote,
+            command: "aws codebuild batch-get-build-batches --ids ${build_id}"
+
+    def json = readJSON text: "" + result
+    return json.buildBatches[0]
+}
