e6194f4ddaef4695024f52ef7c039e52a118d414
Simon Kepplinger <simon.kepplinger@valcome.at>
2023-06-29 13:25:26 +0200

    [NWS-1409] Change 'Valcome Analytics' to 'Valcome Innovation'

--- vars/gitPushTag.groovy
+++ vars/gitPushTag.groovy
@@ -2,7 +2,7 @@ def call(version, key) {
     sshagent(credentials: ["${key}"]) {
         checkout scm
         sh 'git config --global user.email "dev@valcome.at"'
-        sh 'git config --global user.name "valcome-analytics"'
+        sh 'git config --global user.name "valcome-innovation"'
         sh "git tag ${version} || true"
         sh "git push origin ${version}"
     }
