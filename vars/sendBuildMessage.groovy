deea11f4a307306d4007aeddfaed73a57a45b237
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2023-09-18 18:03:48 +0200

    Remove tabs in front of google message

--- vars/sendBuildMessage.groovy
+++ vars/sendBuildMessage.groovy
@@ -1,13 +1,13 @@
 def call(msg, envs = '') {
     def message = """
-        *${env.JOB_BASE_NAME} #${env.BUILD_NUMBER}*
-        ${currentBuild.currentResult}
-        ${msg}
+    *${env.JOB_BASE_NAME} #${env.BUILD_NUMBER}*
+    ${currentBuild.currentResult}
+    ${msg}
 
-        _finished in ${currentBuild.durationString.replace(' and counting', '')}_
+    _finished in ${currentBuild.durationString.replace(' and counting', '')}_
 
-        ${envs}
-        ${env.BUILD_URL}
+    ${envs}
+    ${env.BUILD_URL}
     """
 
     sendGoogleChatMessage(message)
