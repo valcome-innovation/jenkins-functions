a6c1abd2529c8f1cc254d969dce7cde19ba4a9fd
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2023-11-14 13:03:29 +0100

    Add s3 deployment class

---
+++ src/org/valcome/AWSS3Deployment.groovy
@@ -0,0 +1,43 @@
+package org.valcome
+
+class AWSS3Deployment implements Serializable {
+
+    def steps
+    def params
+
+    AWSS3Deployment(steps = null,
+                    image = null,
+                    environment = null,
+                    version = null
+    ) {
+        this.steps = steps
+        this.params = [
+            image: image,
+            env: environment,
+            version: version
+        ]
+    }
+
+    def deploy(srcBucket, destBucket) {
+        def deployCommand = """
+        aws s3 cp \
+        s3://${srcBucket}/artifacts/${params.image}/${params.env}/${params.version} \
+        s3://${destBucket}/ \
+        --recursive
+        """
+
+        steps.sh script: "${deployCommand}", returnStdout: true
+    }
+
+    def invalidateCloudfrontCaches(cloudfrontId) {
+        def invalidateCommand = """
+        aws cloudfront create-invalidation \
+        --distribution-id ${cloudfrontId} \
+        --paths / \
+        """
+
+        steps.sh script: "${invalidateCommand}", returnStdout: true
+
+        steps.sleep 15
+    }
+}
