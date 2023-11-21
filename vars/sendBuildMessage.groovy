5e715a1daafdf87d2bf3f4fdf4729dc548bcf0b7
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2023-11-21 12:36:29 +0100

    improve sendBuildMessage

--- vars/sendBuildMessage.groovy
+++ vars/sendBuildMessage.groovy
@@ -1,13 +1,12 @@
 def call(msg, envs = '') {
     def message = """
     *${env.JOB_BASE_NAME} #${env.BUILD_NUMBER}*
+    ${env.BUILD_URL}
+    _finished in ${currentBuild.durationString.replace(' and counting', '')}_
     ${currentBuild.currentResult}
     ${msg}
 
-    _finished in ${currentBuild.durationString.replace(' and counting', '')}_
-
     ${envs}
-    ${env.BUILD_URL}
     """
 
     sendGoogleChatMessage(message)
