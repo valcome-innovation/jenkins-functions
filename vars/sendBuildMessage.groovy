15aea00082cf74c289aadf6930b551cdb0faa31d
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2023-11-21 13:44:27 +0100

    sendBuildMessage formatting

--- vars/sendBuildMessage.groovy
+++ vars/sendBuildMessage.groovy
@@ -1,6 +1,6 @@
 def call(msg, envs = '') {
     def message = """
-    *${env.JOB_BASE_NAME} #${env.BUILD_NUMBER}* _${currentBuild.currentResult} after ${currentBuild.durationString.replace(' and counting', '')}_
+    *${env.JOB_BASE_NAME} #${env.BUILD_NUMBER}* - _${currentBuild.currentResult} after ${currentBuild.durationString.replace(' and counting', '')}_
     ${env.BUILD_URL}
     ${msg}
     ${envs}
