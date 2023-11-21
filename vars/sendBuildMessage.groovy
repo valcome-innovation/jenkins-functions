863d68d0afeb2a68488fc90dcbc9bbac9f46a69b
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2023-11-21 14:03:25 +0100

    fix syntax error

--- vars/sendBuildMessage.groovy
+++ vars/sendBuildMessage.groovy
@@ -3,7 +3,6 @@ def call(msg, envs = '') {
     *${env.JOB_BASE_NAME} <${env.BUILD_URL}|#${env.BUILD_NUMBER}>* - ${currentBuild.currentResult} after ${currentBuild.durationString.replace(' and counting', '')}
     ${msg}
     ${envs}
-    <font color=\"#FF0000\">RED</font>
     """
 
     sendGoogleChatMessage(message)
