c1b4edacd7f18719484093579dc22171fdcca1d1
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2023-11-21 14:01:03 +0100

    test

--- vars/sendBuildMessage.groovy
+++ vars/sendBuildMessage.groovy
@@ -3,6 +3,7 @@ def call(msg, envs = '') {
     *${env.JOB_BASE_NAME} <${env.BUILD_URL}|#${env.BUILD_NUMBER}>* - ${currentBuild.currentResult} after ${currentBuild.durationString.replace(' and counting', '')}
     ${msg}
     ${envs}
+    <font color=\"#FF0000\">RED</font>
     """
 
     sendGoogleChatMessage(message)
