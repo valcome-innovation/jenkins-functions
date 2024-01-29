9c996b386160df1a07bc7933390c5d0e7f7ab23a
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2024-01-29 10:21:55 +0100

    fix writeMapToFile syntax

--- vars/writeMapToFile.groovy
+++ vars/writeMapToFile.groovy
@@ -2,7 +2,5 @@ def call(map, filename) {
     def envLines = map.collect { key, value -> "$key=$value" }
     def envContent = envLines.join('\n')
 
-    writeFile
-        text: "${envContent}\n",
-        file: filename
+    writeFile text: "${envContent}\n", file: filename
 }
