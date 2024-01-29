4a19fe858b12636a821e962779fe57562531759c
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2024-01-29 09:44:08 +0100

    writeMapToFile

--- vars/writeMapToFile.groovy
+++ vars/writeMapToFile.groovy
@@ -1,6 +1,8 @@
-def call(map, file) {
+def call(map, filename) {
     def envLines = map.collect { key, value -> "$key=$value" }
     def envContent = envLines.join('\n')
 
-    writeFile text: envContent, file: file
+    writeFile
+        text: "${envContent}\n",
+        file: filename
 }
