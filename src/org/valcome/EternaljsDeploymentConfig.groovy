2991f37450d6a42300c7667981e300ee7976f56c
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2024-01-30 15:49:07 +0100

    [EJS-18] Draft EternaljsDeploymentConfig

---
+++ src/org/valcome/EternaljsDeploymentConfig.groovy
@@ -0,0 +1,20 @@
+
+class EternaljsDeploymentConfig implements Serializable {
+
+    def steps
+
+    EternaljsDeploymentConfig(steps) {
+        this.steps = steps
+    }
+
+    def parse() {
+        if (!steps.env.DEPLOYMENT_CONFIG) {
+            steps.error "No deployment configuration JSON provided"
+        }
+
+        steps.withFileParameter("DEPLOYMENT_CONFIG") {
+            def deploymentConfigJSON = steps.readJSON file: steps.env.DEPLOYMENT_CONFIG
+            return deploymentConfigJSON
+        }
+    }
+}
