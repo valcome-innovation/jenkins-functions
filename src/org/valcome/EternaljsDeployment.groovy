68aaf7d220ee91ce4eca5bae1dce9d3fa22c7c13
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2024-01-30 10:02:00 +0100

    Add deployAllServices method to eternaljs deployment

--- src/org/valcome/EternaljsDeployment.groovy
+++ src/org/valcome/EternaljsDeployment.groovy
@@ -12,16 +12,14 @@ class EternaljsDeployment implements Serializable {
 
     }
 
-    def deployService(String service) {
+    def deployAllServices() {
         this.validateRequiredJobParams()
 
-        def deploymentConfigJson = steps.readJSON file: deploymentConfigPath
-        String deploymentConfigContent = steps.readFile file: deploymentConfigPath
-        String base64Content = this.getBase64Config(deploymentConfigContent)
+        def deploymentConfigJson = this.getDeploymentConfigJSON()
+        String base64Content = this.getBase64Config()
 
-        steps.build job: 'eternal.js/service/deploy',
+        steps.build job: 'eternal.js/service/deploy-all',
                 parameters: [
-                        steps.string(name: 'APP', value: "${service}"),
                         steps.string(name: 'VERSION', value: "${steps.env.VERSION}"),
                         steps.string(name: 'ZONE', value: "${steps.env.ZONE}"),
                         steps.string(name: 'HOST', value: "${steps.env.HOST}"),
@@ -33,7 +31,33 @@ class EternaljsDeployment implements Serializable {
                 ]
     }
 
-    String getBase64Config(String deploymentConfigContent) {
+    def deployService(String service) {
+        this.validateRequiredJobParams()
+
+        def deploymentConfigJson = this.getDeploymentConfigJSON()
+        String base64Content = this.getBase64Config()
+
+        steps.build job: 'eternal.js/service/deploy',
+            parameters: [
+                steps.string(name: 'APP', value: "${service}"),
+                steps.string(name: 'VERSION', value: "${steps.env.VERSION}"),
+                steps.string(name: 'ZONE', value: "${steps.env.ZONE}"),
+                steps.string(name: 'HOST', value: "${steps.env.HOST}"),
+                steps.string(name: 'SSH', value: "${steps.env.SSH}"),
+                steps.string(name: 'VAULT_APP_ROLE_SECRET_ID', value: "${steps.env.VAULT_CREDENTIALS_ID}"),
+                steps.string(name: 'PROJECT', value: "${deploymentConfigJson.project}"),
+                steps.string(name: 'BRANCH', value: 'main'),
+                steps.base64File(name: 'DEPLOYMENT_CONFIG', base64: base64Content)
+            ]
+    }
+
+    String getDeploymentConfigJSON() {
+        def json = steps.readJSON file: deploymentConfigPath
+        return json
+    }
+
+    String getBase64Config() {
+        String deploymentConfigContent = steps.readFile file: deploymentConfigPath
         return Base64.encoder.encodeToString(deploymentConfigContent.bytes)
     }
 
