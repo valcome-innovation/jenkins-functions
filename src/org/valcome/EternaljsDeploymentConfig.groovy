e5c9c61be30d4cbfa67e6791b6fc9c4fda6a3ea4
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2024-01-30 16:04:10 +0100

    [EJS-18] fix return value of parse()

--- src/org/valcome/EternaljsDeploymentConfig.groovy
+++ src/org/valcome/EternaljsDeploymentConfig.groovy
@@ -9,35 +9,35 @@ class EternaljsDeploymentConfig implements Serializable {
         this.steps = steps
     }
 
-    def parse() {
+    EternaljsDeploymentConfig parse() {
         if (!steps.env.DEPLOYMENT_CONFIG) {
             steps.error "No deployment configuration JSON provided"
         }
 
         steps.withFileParameter("DEPLOYMENT_CONFIG") {
-            config = steps.readJSON file: steps.env.DEPLOYMENT_CONFIG
-            return config
+            this.config = steps.readJSON file: steps.env.DEPLOYMENT_CONFIG
+            return this
         }
     }
 
     String getProject() {
-        return config.project
+        return this.config.project
     }
 
     String getZone() {
-        return config.zone
+        return this.config.zone
     }
 
     def getServices() {
-        return config.services
+        return this.config.services
     }
 
     def getService(String app) {
-        return getServices().find { it.app == app }
+        return this.getServices().find { it.app == app }
     }
 
     def getServiceDeploymentConfig(String app) {
-        return getService(app).deployment.collectEntries { key, value ->
+        return this.getService(app).deployment.collectEntries { key, value ->
             [(key.toString().toUpperCase()): value]
         }
     }
