a61f6c82e750245469427dcb664496f64fbc3dde
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2024-02-13 15:52:49 +0100

    [EJS-72] remove useless linebreaks

--- vars/sendBuildMessage.groovy
+++ vars/sendBuildMessage.groovy
@@ -1,6 +1,5 @@
 def call(msg, envs = '') {
-    def message = """*${env.JOB_BASE_NAME} <${env.BUILD_URL}|#${env.BUILD_NUMBER}>* - ${currentBuild.currentResult} after ${currentBuild.durationString.replace(' and counting', '')}
-${msg}${envs ? "\n" : ""}${envs}
+    def message = """*${env.JOB_BASE_NAME} <${env.BUILD_URL}|#${env.BUILD_NUMBER}>* - ${currentBuild.currentResult} after ${currentBuild.durationString.replace(' and counting', '')}${msg ? "\n" : ""}${msg}${envs ? "\n" : ""}${envs}
 """
 
     sendGoogleChatMessage(message)
