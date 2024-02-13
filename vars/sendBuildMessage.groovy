3a908aff799b1cac1a8e617de23534c6c1a66ee0
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2024-02-13 15:43:55 +0100

    [EJS-72] fix build message indents

--- vars/sendBuildMessage.groovy
+++ vars/sendBuildMessage.groovy
@@ -1,9 +1,7 @@
 def call(msg, envs = '') {
-    def message = """
-    *${env.JOB_BASE_NAME} <${env.BUILD_URL}|#${env.BUILD_NUMBER}>* - ${currentBuild.currentResult} after ${currentBuild.durationString.replace(' and counting', '')}
-    ${msg}
-    ${envs}
-    """
+    def message = """*${env.JOB_BASE_NAME} <${env.BUILD_URL}|#${env.BUILD_NUMBER}>* - ${currentBuild.currentResult} after ${currentBuild.durationString.replace(' and counting', '')}
+${msg}${envs ? "\n" : ""}${envs}
+"""
 
     sendGoogleChatMessage(message)
 }
