212d0d65b70885d6dfa6a33ebbc78e07bf50e690
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2025-02-04 11:09:50 +0100

    Add AWSLambdaFunctionDeployment

---
+++ src/org/valcome/AWSLambdaFunctionDeployment.groovy
@@ -0,0 +1,52 @@
+package org.valcome
+
+class AWSLambdaFunctionDeployment implements Serializable {
+    def steps
+    String function
+    String codeFilePath
+
+    AWSLambdaFunctionDeployment(steps = null,
+                                function = null,
+                                codeFilePath = null) {
+        this.steps = steps
+        this.function = function
+        this.codeFilePath = codeFilePath
+    }
+
+    public def deployWithTests(closure) {
+        updateFunction()
+
+        closure()
+
+        publishVersion()
+    }
+
+    public def updateFunction() {
+        steps.sh script: """
+        aws lambda update-function-code \
+        --function-name ${function} \
+        --zip-file fileb://${codeFilePath}
+        """
+    }
+
+    public def runTestEvent(String testEventFilePath) {
+        String jsonTextOutput = steps.sh script: """
+        aws lambda invoke \
+        --function-name ${function} \
+        --payload file://${testEventFilePath} \
+        """, returnStdout: true
+
+        def jsonTestOutput = steps.readJSON text: jsonTextOutput
+
+        assert jsonTestOutput.TestResult.FunctionErrorMessage == ""
+
+        return jsonTestOutput
+    }
+
+    public def publishVersion() {
+        steps.sh script: """
+        aws lambda publish-version \
+        --function-name ${function} \
+        """
+    }
+}
