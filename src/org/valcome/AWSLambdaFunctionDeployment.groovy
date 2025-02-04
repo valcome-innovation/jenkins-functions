8c219130b51c6574c49d1d99242630dfad3798ff
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2025-02-04 12:47:02 +0100

    [lambda] debug

--- src/org/valcome/AWSLambdaFunctionDeployment.groovy
+++ src/org/valcome/AWSLambdaFunctionDeployment.groovy
@@ -45,6 +45,7 @@ class AWSLambdaFunctionDeployment implements Serializable {
         """, returnStdout: true
 
         steps.sh "cat test-output.json"
+        steps.sh "cat ${jsonTextOutput}"
 
         def jsonTestOutput = steps.readJSON file: 'test-output.json'
         return jsonTestOutput
