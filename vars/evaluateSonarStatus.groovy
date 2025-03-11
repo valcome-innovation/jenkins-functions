7119f5ea0eb26c1bb6f113b6e0211ed79a024331
Philipp Kepplinger <philipp.kepplinger@valcome.at>
2025-03-11 17:30:39 +0100

    [FAI-234] finish sonar status update check

--- vars/evaluateSonarStatus.groovy
+++ vars/evaluateSonarStatus.groovy
@@ -3,18 +3,19 @@ def call(String projectName) {
         string(credentialsId: 'SONAR_TOKEN', variable: 'SONAR_TOKEN')
     ]) {
         def authHeader = 'Authorization: Bearer $SONAR_TOKEN'
-        def curl = "curl -sS -H $authHeader https://sonar.valcome.dev/api/qualitygates/project_status?projectKey=$projectName"
+        def curl = """curl -sS -H "$authHeader" 'https://sonar.valcome.dev/api/qualitygates/project_status?projectKey=$projectName'"""
         String response = sh script: "$curl",
                 returnStdout: true
-        sh "echo $response"
 
         def responseJSON = readJSON text: response
+        def sonarStatus = responseJSON.projectStatus.status
+        echo "SONAR analysis status is: $sonarStatus"
 
-        if (responseJSON.projectStatus.status == "ERROR") {
-            def detailsUrl = "https://sonar.valcome.dev/dashboard?id=${projectName}"
+        if (responseJSON.projectStatus.status != "ERROR") {
+            def detailsUrl = "https://sonar.valcome.dev/dashboard?id=$projectName"
             publishGithubCheck(
                     "Sonar",
-                    "Sonar",
+                    "Analysis status: $sonarStatus",
                     "COMPLETED",
                     "NEUTRAL",
                     "SONAR analysis resulted in ERROR state. Please check the details",
